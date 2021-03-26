c7f1b63e929a521439b52fb13c0c76b14555034d
edurso <eric@casadurso.com>
2021-03-26 08:05:22 -0400

    some updates to ui - still needs to be tested

--- application.py
+++ application.py
@@ -15,13 +15,19 @@ class App:
         self.window.title = title
         self.drone = drone
         self.video = DroneVideoCapture(self.drone)
-        self.canvas = tk.Canvas(window, width = self.vid.width, height = self.vid.height)
+        self.canvas = tk.Canvas(window, width=self.vid.width, height=self.vid.height)
         self.canvas.pack()
         self.delay = 15
-        self.update_video()
-        init()
-        self.drive_thread = threading.Thread(target=self.keyboard_control)
-        self.drive_thread.start()
+        self.vid_thread = threading.Thread(target=self.update_video)
+        self.vid_thread.start()
+        self.btn_snapshot=tk.Button(window, text="Drive w/ Keyboard", width=50, command=self.enter_keyboard_control)
+        self.btn_snapshot.pack(anchor=tk.CENTER, expand=True)
+        self.btn_takeoff=tk.Button(window, text="Takeoff", width=50, command=self.drone.takeoff)
+        self.btn_takeoff.pack(anchor=tk.CENTER, expand=True)
+        self.btn_land=tk.Button(window, text="Land Drone", width=50, command=self.drone.land)
+        self.btn_land.pack(anchor=tk.CENTER, expand=True)
+        self.btn_estop=tk.Button(window, text="Emergency Stop", width=50, command=self.drone.emergency)
+        self.btn_estop.pack(anchor=tk.CENTER, expand=True)
         self.window.mainloop()
 
     def update_video(self):
@@ -29,9 +35,14 @@ class App:
         if ret:
             self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
             self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        self.window.after(self.delay, self.update)
+        self.window.after(self.delay, self.update_video)
 
-    def keyboard_control(self):
+    def enter_keyboard_control(self):
+        init()
+        self.drive_thread = threading.Thread(target=self.keyboard_control_loop)
+        self.drive_thread.start()
+
+    def keyboard_control_loop(self):
         while True:
             inp = getkeyinp()
             self.drone.send_rc_control(inp[0], inp[1], inp[2], inp[3])

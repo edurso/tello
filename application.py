e73164f1621779049caca0876a87c51277d9870c
edurso <eric@casadurso.com>
2021-06-08 11:01:19 -0400

    update to get gamepad to work

--- application.py
+++ application.py
@@ -3,9 +3,10 @@ import tkinter as tk
 import threading
 import cv2
 import PIL
-import gamepad as gp
+import PIL.Image, PIL.ImageTk
 from dronevision import DroneVideoCapture
 from drone import new_drone
+from inputs import get_gamepad
 
 # define application space
 class App:
@@ -17,24 +18,20 @@ class App:
         self.vid = DroneVideoCapture(self.drone)
         self.canvas = tk.Canvas(window, width=self.vid.width, height=self.vid.height)
         self.canvas.pack()
-        self.delay = 15
+        self.delay = 120
 
         # Start Video Thread
-        self.vid_thread = threading.Thread(target=self.update_video)
-        self.vid_thread.start()
+        # self.update_video()
 
         # Start Control Thread
-        self.control_thread = threading.Thread(target=self.joystick_control_loop)
-        self.control_thread.start()
+        self.joystick_control_loop()
 
         # Quit Button
         self.btn_snapshot=tk.Button(window, text="Quit", width=50, command=self.toggle_running)
         self.btn_snapshot.pack(anchor=tk.CENTER, expand=True)
-        self.run = True
-        self.fly = False
 
         # Run Loop in Main Thread
-        self.window.mainloop()
+        #self.window.mainloop()
 
     def update_video(self):
         ret, frame = self.vid.get_frame()
@@ -47,7 +44,45 @@ class App:
         self.run = not self.run
 
     def joystick_control_loop(self):
-        gp.joystick_control_loop(self)
+        fwd_rev = 0
+        left_right = 0
+        up_down = 0
+        rot = 0
+        run = True
+        fly = False
+        while run:
+            events = get_gamepad()
+            for event in events:
+                if not event.ev_type == 'Sync':
+                    #print('EVENT: {} | {} | {}'.format(event.ev_type, event.code, event.state))
+                    if event.code == 'ABS_Y':
+                        fwd_rev = self.scale_js(int(event.state))
+                    if event.code == 'ABS_X':
+                        left_right = self.scale_js(int(event.state))
+                    if event.code == 'ABS_RY':
+                        up_down = self.scale_js(int(event.state))
+                    if event.code == 'ABS_RX':
+                        rot = self.scale_js(int(event.state))
+                    if event.code == 'BTN_START':
+                        if event.state == 1:
+                            fly = not fly
+                            if fly:
+                                self.drone.takeoff()
+                            else:
+                                self.drone.land()
+                    if event.code == 'BTN_TR' and event.state == 1:
+                        self.drone.flip_right()
+                    if event.code == 'BTN_TL' and event.state == 1:
+                        self.drone.flip_left()
+
+            if fly:            
+                self.drone.send_rc_control(left_right, fwd_rev, up_down, rot)
+
+    def scale_js(self, val):
+        inputRange = 32768 - (-32767)
+        outputRange = 100 - (-100)
+        val = int((val - (-32767)) * outputRange / inputRange + (-100))
+        return val if abs(val) > 30 else 0
 
     def __del__(self):
         return
--- gamepad.py
+++ gamepad.py
@@ -1,43 +1,45 @@
 from __future__ import print_function
 from inputs import get_gamepad
-from application import App
 
-def joystick_control_loop(app: App):
+def joystick_control_loop():
     fwd_rev = 0
     left_right = 0
     up_down = 0
     rot = 0
-    while app.run:
+    run = True
+    fly = False
+    while run:
         events = get_gamepad()
         for event in events:
             if not event.ev_type == 'Sync':
-                print('EVENT: {} | {} | {}'.format(event.ev_type, event.code, event.state))
+                #print('EVENT: {} | {} | {}'.format(event.ev_type, event.code, event.state))
                 if event.code == 'ABS_Y':
-                    fwd_rev = scale_js(event.state)
+                    fwd_rev = scale_js(int(event.state))
                 if event.code == 'ABS_X':
-                    left_right = scale_js(event.state)
+                    left_right = scale_js(int(event.state))
                 if event.code == 'ABS_RY':
-                    up_down = scale_js(event.state)
+                    up_down = scale_js(int(event.state))
                 if event.code == 'ABS_RX':
-                    rot = scale_js(event.state)
+                    rot = scale_js(int(event.state))
                 if event.code == 'BTN_START':
                     if event.state == 1:
-                        app.fly = not app.fly
-                        if app.fly:
-                            app.drone.takeoff()
+                        fly = not fly
+                        if fly:
+                            print('takeoff') # app.drone.takeoff()
                         else:
-                            app.drone.land()
+                            print('land') # app.drone.land()
                 if event.code == 'BTN_TR' and event.state == 1:
-                    app.drone.flip_right()
+                    print('flip right') # app.drone.flip_right()
                 if event.code == 'BTN_TL' and event.state == 1:
-                    app.drone.flip_left()
+                    print('flip left') # app.drone.flip_left()
 
-        if app.fly:            
-            app.drone.send_rc_control(left_right, fwd_rev, up_down, rot)
+        if fly:            
+            print('{} {} {} {}'.format(left_right, fwd_rev, up_down, rot)) # app.drone.send_rc_control(left_right, fwd_rev, up_down, rot)
 
 def scale_js(val):
-    max = 100
-    min = 0
-    sign = -1 if val < 0 else 1
-    val = abs(val) % ( max - min + 1) + min
-    return val * sign
+    inputRange = 32768 - (-32767)
+    outputRange = 100 - (-100)
+    val = int((val - (-32767)) * outputRange / inputRange + (-100))
+    return val if abs(val) > 30 else 0
+
+joystick_control_loop()

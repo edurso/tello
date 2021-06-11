c638450bc310396ad1ca9edf429f7ca85e934ea6
edurso <eric@casadurso.com>
2021-06-11 08:08:13 -0400

    remove unnecessary tkinter windows - use imshow instead

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+from time import sleep
 from djitellopy import Tello
 import tkinter as tk
 import threading
@@ -11,50 +12,35 @@ from inputs import get_gamepad
 # define application space
 class App:
     
-    def __init__(self, window, title, drone: Tello):
-        self.window = window
-        self.window.title = title
+    def __init__(self, drone: Tello):
         self.drone = drone
-        self.vid = DroneVideoCapture(self.drone)
-        self.canvas = tk.Canvas(window, width=self.vid.width, height=self.vid.height)
-        self.canvas.pack()
-        self.delay = 120
+        threading.Thread(target=self.video).start()
+        self.run = True
 
-        # Start Video Thread
-        # self.update_video()
-
-        # Start Control Thread
+        # Start Control Loop
         self.joystick_control_loop()
 
-        # Quit Button
-        self.btn_snapshot=tk.Button(window, text="Quit", width=50, command=self.toggle_running)
-        self.btn_snapshot.pack(anchor=tk.CENTER, expand=True)
 
-        # Run Loop in Main Thread
-        #self.window.mainloop()
-
-    def update_video(self):
+    def video(self):
+        self.vid = DroneVideoCapture(self.drone)
         ret, frame = self.vid.get_frame()
-        if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        self.window.after(self.delay, self.update_video)
-
-    def toggle_running(self):
-        self.run = not self.run
+        while ret and self.run:
+            cv2.imshow('Tello Drone Control', frame)  
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                self.run = False
+                break
+            #sleep(50)  
 
     def joystick_control_loop(self):
         fwd_rev = 0
         left_right = 0
         up_down = 0
         rot = 0
-        run = True
         fly = False
-        while run:
+        while self.run:
             events = get_gamepad()
             for event in events:
                 if not event.ev_type == 'Sync':
-                    #print('EVENT: {} | {} | {}'.format(event.ev_type, event.code, event.state))
                     if event.code == 'ABS_Y':
                         fwd_rev = self.scale_js(int(event.state))
                     if event.code == 'ABS_X':
@@ -70,13 +56,15 @@ class App:
                                 self.drone.takeoff()
                             else:
                                 self.drone.land()
-                    if event.code == 'BTN_TR' and event.state == 1:
+                                self.run = False
+                    if event.code == 'BTN_TR' and event.state == 1 and fly:
                         self.drone.flip_right()
-                    if event.code == 'BTN_TL' and event.state == 1:
+                    if event.code == 'BTN_TL' and event.state == 1 and fly:
                         self.drone.flip_left()
-
             if fly:            
                 self.drone.send_rc_control(left_right, fwd_rev, up_down, rot)
+            
+            #sleep(20)
 
     def scale_js(self, val):
         inputRange = 32768 - (-32767)
@@ -87,5 +75,4 @@ class App:
     def __del__(self):
         return
 
-drone = new_drone()
-App(tk.Tk(), 'Tello Drone Controller', drone)
+App(new_drone())
